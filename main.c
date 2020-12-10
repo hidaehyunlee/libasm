19d91fd93f4e56a838d1c0aa401c71b7836f263e
Daehyun <leehi0210@gmail.com>
2020-12-10 12:49:46 +0900

    Done Noe

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: daelee <daelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 12:44:56 by daelee            #+#    #+#             */
-/*   Updated: 2020/12/10 12:44:57 by daelee           ###   ########.fr       */
+/*   Updated: 2020/12/10 12:48:57 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,17 @@ int	main(void)
 	printf(CYAN"[case 2] "RESET"ft_strlen = %zu\n", ft_strlen("daelee!"));
 	printf(BLUE"______TESTING <ft_strcmp>______\n"RESET);
 	printf(CYAN"[case 1] "RESET"ft_strcpy = %s\n", ft_strcpy(s, ""));
-	printf(CYAN"[case 2] "RESET"ft_strcpy = %s\n", ft_strcpy(s ,"daelee!"));
+	printf(CYAN"[case 2] "RESET"ft_strcpy = %s\n", ft_strcpy(s, "daelee!"));
 	printf(BLUE"______TESTING <ft_strcmp>______\n"RESET);
 	printf(CYAN"[case 1] "RESET"ft_strcmp = %d\n", ft_strcmp("a", "a"));
-	printf(CYAN"[case 2] "RESET"ft_strcmp = %d\n", ft_strcmp("a" ,"b"));
+	printf(CYAN"[case 2] "RESET"ft_strcmp = %d\n", ft_strcmp("a", "b"));
 	printf(BLUE"______TESTING <ft_wrtie>______\n"RESET);
-	printf(CYAN"[case 1] "RESET"ft_write = %zd\n", ft_write(1 , "daelee!", 10));
-	printf(CYAN"[case 2] "RESET"ft_write = %zd\n", ft_write(1 , "daelee!", 3));
+	printf(CYAN"[case 1] "RESET"ft_write = %zd\n", ft_write(1, "daelee!", 10));
+	printf(CYAN"[case 2] "RESET"ft_write = %zd\n", ft_write(1, "daelee!", 3));
 	printf(BLUE"______TESTING <ft_read>______\n"RESET);
-	printf(CYAN"[case 1] "RESET"ft_read = %zd\n", ft_read(0 , s, 20));
+	printf(CYAN"[case 1] "RESET"ft_read = %zd\n", ft_read(0, s, 20));
 	printf(BLUE"______TESTING <ft_strdup>______\n"RESET);
 	printf(CYAN"[case 1] "RESET"ft_strdup = %s\n", ft_strdup(""));
 	printf(CYAN"[case 2] "RESET"ft_strdup = %s\n", ft_strdup("daelee!"));
-
 	return (1);
 }
