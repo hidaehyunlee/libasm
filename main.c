06985f386aaf742190a86d816a48f781f946bc40
Daehyun Lee <leehi0210@gmail.com>
2020-12-10 12:34:57 +0900

    Check Norme

--- main.c
+++ main.c
@@ -1,34 +1,37 @@
 #include "libasm.h"
-# define BLUE "\x1b[34m"
-# define CYAN "\x1b[36m"
-# define RESET "\x1b[0m"
 
 int main()
 {
 	char s[50];
 
 	printf(BLUE"\n______TESTING <ft_strlen>______\n"RESET);
-	printf(CYAN"[case 1] "RESET"ft_strlen = %zu | strlen = %zu\n", ft_strlen(""), strlen(""));
-	printf(CYAN"[case 2] "RESET"ft_strlen = %zu | strlen = %zu\n", ft_strlen("Hello, World!"), strlen("Hello, World!"));
-
+	printf(CYAN"[case 1] "RESET"ft_strlen = %zu | ", ft_strlen(""));
+	printf("strlen = %zu\n", strlen(""));
+	printf(CYAN"[case 2] "RESET"ft_strlen = %zu | ", ft_strlen("Hello, World!"));
+	printf("strlen = %zu\n", strlen("Hello, World!"));
 	printf(BLUE"\n______TESTING <ft_strcmp>______\n"RESET);
-	printf(CYAN"[case 1] "RESET"ft_strcpy = %s | strcpy = %s\n", ft_strcpy(s, ""), strcpy(s, ""));
-	printf(CYAN"[case 2] "RESET"ft_strcpy = %s | strcpy = %s\n", ft_strcpy(s, "Hello, World!"), strcpy(s, "Hello, World!"));
-
+	printf(CYAN"[case 1] "RESET"ft_strcpy = %s | " ft_strcpy(s, ""));
+	printf("strcpy = %s\n", strcpy(s, ""));
+	printf(CYAN"[case 2] "RESET"ft_strcpy = %s | ", ft_strcpy(s, "Hello, World!"));
+	printf("strcpy = %s\n", strcpy(s, "Hello, World!"));
 	printf(BLUE"\n______TESTING <ft_strcmp>______\n"RESET);
-	printf(CYAN"[case 1] "RESET"ft_strcmp = %d | strcmp = %d\n", ft_strcmp("a", "a"), strcmp("a", "a"));
-	printf(CYAN"[case 2] "RESET"ft_strcmp = %d | strcmp = %d\n", ft_strcmp("a" ,"b"), strcmp("a", "b"));
-
+	printf(CYAN"[case 1] "RESET"ft_strcmp = %d | ", ft_strcmp("a", "a"));
+	printf("strcmp = %d\n", strcmp("a", "a"));
+	printf(CYAN"[case 2] "RESET"ft_strcmp = %d | ", ft_strcmp("a" ,"b"));
+	printf("strcmp = %d\n", strcmp("a", "b"));
 	printf(BLUE"\n______TESTING <ft_wrtie>______\n"RESET);
-	printf(CYAN"[case 1] "RESET"ft_write = %zd | write = %zd\n", ft_write(1 , "Hello, World!", 13), write(1 , "Hello, World!", 13));
-	printf(CYAN"[case 2] "RESET"ft_write = %zd | wrtie = %zd\n", ft_write(1 , "Hello, World!", 8), write(1 , "Hello, World!", 8));
-
+	printf(CYAN"[case 1] "RESET"ft_write = %zd | ", ft_write(1 , "Hello, World!", 13));
+	printf("write = %zd\n", write(1 , "Hello, World!", 13));
+	printf(CYAN"[case 2] "RESET"ft_write = %zd | ", ft_write(1 , "Hello, World!", 8));
+	printf("wrtie = %zd\n", write(1 , "Hello, World!", 8));
 	printf(BLUE"\n______TESTING <ft_read>______\n"RESET);
-	printf(CYAN"[case 1] "RESET"ft_read = %zd | read = %zd\n", ft_read(0 , s, 100), read(0 , s, 100));
-
+	printf(CYAN"[case 1] "RESET"ft_read = %zd | ", ft_read(0 , s, 100));
+	printf("read = %zd\n", read(0 , s, 100));
 	printf(BLUE"\n______TESTING <ft_strdup>______\n"RESET);
-	printf(CYAN"[case 1] "RESET"ft_strdup = %s | strdup = %s\n", ft_strdup(""), strdup(""));
-	printf(CYAN"[case 2] "RESET"ft_strdup = %s | strdup = %s\n", ft_strdup("Hello, World!"), strdup("Hello, World!"));
+	printf(CYAN"[case 1] "RESET"ft_strdup = %s | ", ft_strdup(""));
+	printf("strdup = %s\n", strdup(""));
+	printf(CYAN"[case 2] "RESET"ft_strdup = %s | ", ft_strdup("Hello, World!"));
+	printf("strdup = %s\n", strdup("Hello, World!"));
 
 	return (1);
 }
